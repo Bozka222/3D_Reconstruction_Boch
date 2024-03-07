0beea1307fda984aae8d579ab08244e7f11ee758
JanBoch <bochjan@seznam.cz>
2024-03-07 13:54:44 +0100

    Tested version with .raw saving, testing version with bag

--- depth_map_to_point_cloud.py
+++ depth_map_to_point_cloud.py
@@ -1,49 +1,68 @@
 import open3d as o3d
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import pyrealsense2 as rs
+from bagpy import bagreader
 
-# Load in color and depth image to create the point cloud
-color_raw = o3d.io.read_image("Data/Output/Color_image/Color_image34.jpg")
-depth_raw = o3d.io.read_image("Data/Output/Depth_image/Depth_image34.jpg")
-color = o3d.geometry.Image(np.array(np.asarray(color_raw)[:, :, :3]).astype('uint8'))
-rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color=color, depth=depth_raw,
-                                                                convert_rgb_to_intensity=False,
-                                                                depth_scale=1000,
-                                                                depth_trunc=3.0)
-print(rgbd_image)
+pc = rs.pointcloud()
+points = rs.points()
+depth_filtered_frame = bagreader('Data/Output/PointClouds/3D_Cam/frameset1.bag')
+print(type(depth_filtered_frame))
 
-# Plot the images
-plt.subplot(1, 2, 1)
-plt.title('Color image')
-plt.imshow(rgbd_image.color)
-plt.subplot(1, 2, 2)
-plt.title('Depth image')
-plt.imshow(rgbd_image.depth)
-plt.show()
+points = pc.calculate(depth_filtered_frame)
+vertices = np.asanyarray(points.get_vertices(dims=2))
 
-# Camera intrinsic parameters from camera used to get color and depth images - Camera Calibration
-cv_file = cv2.FileStorage()
-cv_file.open('Data/Input/3D_intrinsic.xml', cv2.FileStorage_READ)
+raw_vertices = np.fromfile("Data/Output/PointClouds/3D_Cam/depth.raw", dtype=np.float32).reshape(-1, 3)
+color_raw = cv2.imread("Data/Output/PointClouds/3D_Cam/color_raw.jpg").reshape(-1, 3)
 
-camera_intrinsic = cv_file.getNode('Intrinsic').mat()
-
-# Set the intrinsic camera parameters
-camera_intrinsic_o3d = o3d.camera.PinholeCameraIntrinsic(width=1280, height=720, fx=camera_intrinsic[0][0],
-                                                         fy=camera_intrinsic[1][1], cx=camera_intrinsic[0][2],
-                                                         cy=camera_intrinsic[1][2])
-print(camera_intrinsic_o3d.intrinsic_matrix)
-
-# Create the point cloud from images and camera intrinsic parameters
-pcd = o3d.geometry.PointCloud.create_from_rgbd_image(image=rgbd_image, intrinsic=camera_intrinsic_o3d)
-# pc = o3d.geometry.PointCloud.create_from_depth_image(image=depth_raw, intrinsic=camera_intrinsic_o3d,
-# depth_scale=1000)
-
-# Flip it, otherwise the point cloud will be upside down
-# o3d.geometry.PointCloud.remove_radius_outlier(pcd, 2, 5, True)
-
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(raw_vertices.astype(np.float64) / 255)
+pcd.colors = o3d.utility.Vector3dVector(color_raw.astype(np.float64) / 255)
 pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
 o3d.visualization.draw_geometries([pcd])
-# pc.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-# o3d.visualization.draw_geometries([pc])
 o3d.io.write_point_cloud("Data/Output/PointClouds/3D_Cam/3D_Camera_PointCloud.ply", pcd, format="ply")
+
+# # Load in color and depth image to create the point cloud
+# color_raw = o3d.io.read_image("Data/Output/Color_image/Color_image34.jpg")
+# depth_raw = o3d.io.read_image("Data/Output/Depth_image/Depth_image34.jpg")
+# color = o3d.geometry.Image(np.array(np.asarray(color_raw)[:, :, :3]).astype('uint8'))
+# rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color=color, depth=depth_raw,
+#                                                                 convert_rgb_to_intensity=False,
+#                                                                 depth_scale=1000,
+#                                                                 depth_trunc=3.0)
+# print(rgbd_image)
+#
+# # Plot the images
+# plt.subplot(1, 2, 1)
+# plt.title('Color image')
+# plt.imshow(rgbd_image.color)
+# plt.subplot(1, 2, 2)
+# plt.title('Depth image')
+# plt.imshow(rgbd_image.depth)
+# plt.show()
+#
+# # Camera intrinsic parameters from camera used to get color and depth images - Camera Calibration
+# cv_file = cv2.FileStorage()
+# cv_file.open('Data/Input/3D_intrinsic.xml', cv2.FileStorage_READ)
+#
+# camera_intrinsic = cv_file.getNode('Intrinsic').mat()
+#
+# # Set the intrinsic camera parameters
+# camera_intrinsic_o3d = o3d.camera.PinholeCameraIntrinsic(width=1280, height=720, fx=camera_intrinsic[0][0],
+#                                                          fy=camera_intrinsic[1][1], cx=camera_intrinsic[0][2],
+#                                                          cy=camera_intrinsic[1][2])
+# print(camera_intrinsic_o3d.intrinsic_matrix)
+#
+# # Create the point cloud from images and camera intrinsic parameters
+# pcd = o3d.geometry.PointCloud.create_from_rgbd_image(image=rgbd_image, intrinsic=camera_intrinsic_o3d)
+# # pc = o3d.geometry.PointCloud.create_from_depth_image(image=depth_raw, intrinsic=camera_intrinsic_o3d,
+# # depth_scale=1000)
+#
+# # Flip it, otherwise the point cloud will be upside down
+# # o3d.geometry.PointCloud.remove_radius_outlier(pcd, 2, 5, True)
+#
+# pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+# o3d.visualization.draw_geometries([pcd])
+# # pc.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+# # o3d.visualization.draw_geometries([pc])
+# o3d.io.write_point_cloud("Data/Output/PointClouds/3D_Cam/3D_Camera_PointCloud.ply", pcd, format="ply")
--- testing.py
+++ testing.py
@@ -1,10 +1,10 @@
 import pyrealsense2 as rs
 import cv2
-import open3d as o3d
 import numpy as np
 
 pc = rs.pointcloud()
 points = rs.points()
+saver = rs.save_single_frameset("Data/Output/PointClouds/3D_Cam/frameset")
 
 # Declare RealSense pipeline, encapsulating the actual device and sensors
 pipeline = rs.pipeline()
@@ -15,8 +15,6 @@ pipeline_wrapper = rs.pipeline_wrapper(pipeline)
 pipeline_profile = config.resolve(pipeline_wrapper)
 device = pipeline_profile.get_device()
 depth_sensor = device.first_depth_sensor()
-# Get depth scale of the device
-depth_scale = depth_sensor.get_depth_scale()
 
 # Enable depth stream
 config.enable_stream(rs.stream.color, 1280, 720, format=rs.format.bgr8, framerate=30)
@@ -24,12 +22,6 @@ config.enable_stream(rs.stream.depth, 1280, 720, format=rs.format.z16, framerate
 
 # Start streaming with chosen configuration
 profile = pipeline.start(config)
-align_to = rs.stream.color
-align = rs.align(align_to)
-
-# We'll use the colorizer to generate texture for our PLY
-# (alternatively, texture can be obtained from color or infrared stream)
-colorizer = rs.colorizer(1)
 
 spat_filter = rs.spatial_filter(0.40, 20.0, 5.0, 0.0)  # Spatial - edge-preserving spatial smoothing
 temp_filter = rs.temporal_filter(0.8, 30.0, 8)  # Temporal - reduces temporal noise
@@ -44,40 +36,18 @@ try:
     filtered = spat_filter.process(depth_raw_frame)
     filtered1 = temp_filter.process(filtered)
     depth_filtered_frame = hole_filter.process(filtered1)
+    saver.process(depth_filtered_frame)
 
     points = pc.calculate(depth_filtered_frame)
     vertices = np.asanyarray(points.get_vertices(dims=2))
-    w = depth_raw_frame.get_width()
-    image_Points = np.reshape(vertices, (-1, w, 3))
 
     depth_image = np.asanyarray(depth_filtered_frame.get_data())
-    color_image = np.asanyarray(color_raw_frame.get_data()).reshape(-1, 3)
+    color_image = np.asanyarray(color_raw_frame.get_data())
+    image = cv2.cvtColor(color_image, cv2.COLOR_RGBA2BGRA)
 
-    pcd = o3d.geometry.PointCloud()
+    vertices.astype("float32").tofile("Data/Output/PointClouds/3D_Cam/depth.raw")
+    cv2.imwrite("Data/Output/PointClouds/3D_Cam/color_raw.jpg", image)
+    cv2.imwrite("Data/Output/PointClouds/3D_Cam/color_image.jpg", color_image)
 
-    pcd.points = o3d.utility.Vector3dVector(vertices.astype(np.float32) / 255)
-    pcd.colors = o3d.utility.Vector3dVector(color_image.astype(np.float64) / 255)
-    o3d.visualization.draw_geometries([pcd])
-    o3d.io.write_point_cloud("Data/Output/PointClouds/3D_Cam/3D_Camera_PointCloud.ply", pcd, format="ply")
-
-    # # Create save_to_ply object
-    # ply = rs.save_to_ply("Data/Output/PointClouds/3D_Cam/3D.ply")
-    #
-    # # Set options to the desired values
-    # # In this example we'll generate a textual PLY with normals (mesh is already created by default)
-    # ply.set_option(rs.save_to_ply.option_ply_binary, False)
-    # ply.set_option(rs.save_to_ply.option_ply_normals, True)
-    # ply.set_option(rs.save_to_ply.option_ply_mesh, False)
-    #
-    # print("Saving to 1.ply...")
-    # # Apply the processing block to the frameset which contains the depth frame and the texture
-    # ply.process(colorized)
-    # print("Done")
-
-    # pc.map_to(color_raw_frame)
-    # points = pc.calculate(depth_raw_frame)
-    # print("Saving to 1.ply...")
-    # points.export_to_ply("Data/Output/PointClouds/3D_Cam/3D.ply", color_raw_frame)
-    # print("Done")
 finally:
     pipeline.stop()
